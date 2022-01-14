44e5e123ca41a3e46d0e175cf61f45bdef48ab52
Elise <elise.piette@insa-lyon.fr>
2022-01-14 16:07:39 +0100

    constructeur vide et str updated

--- string.cpp
+++ string.cpp
@@ -1,5 +1,14 @@
 #include "string.h"
 string::string(){
-  a = nullptr;
-  len=0;
+  _str[0]='\0';
+  _len=0;
+}
+string::string(const char *str){
+  int i=0;
+  while(str[i]!='\0' && i<100){
+    _str[i]=str[i];
+    i++;
+  }
+  this->_str[i]='\0';
+  this->_len=i;
 }
--- string.h
+++ string.h
@@ -1,7 +1,8 @@
 class string {
   protected:
-    char* a;
-    int len;
+    char _str[100]={};
+    int _len;
   public:
     string();
+    string(const char *str);
 };
